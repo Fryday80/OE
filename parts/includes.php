46831be2f43ab97c5fbac29738468ef450f122d3
Fry <Fry.LMU@gmx.de>
2017-12-03 18:26:02 +0100

    shortened css file implemented include file

---
+++ parts/includes.php
@@ -0,0 +1,6 @@
+<?php
+	// you are in  root!
+	require_once ('./nav/nav.inc.php');
+	require_once ('./nav/navbar.php');
+	require_once ('./parts/Events/Entry.php');
+	require_once ('./parts/Events/Events.php');
